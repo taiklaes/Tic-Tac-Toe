8be21beb6bcf1df47d35c84ef5d416e6319b3ad6
Gabriel Santos de Sousa <gabsousa.contato@gmail.com>
2022-01-11 19:11:49 +0000

    fix: mutiples click in the same block

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,50 @@
 $(function(){
-    var jogador = "Um";
-    var state = false;
+    //Var and Const
+    let player = "";
+    let positions = [["", "", ""], ["", "", ""], ["", "", ""],];
 
+    const blocks = [
+        ["A1", "A2", "A3"],
+        ["B1", "B2", "B3"],
+        ["C1", "C2", "C3"],
+    ];
+
+    //Start the game
     $("#play").click(function(){
-        for (let i = 0; i < 3; i++){
-            const l = ["A","B","C"];
+        $("#title").html("The game is started!!! Player one it's your time");
 
-            for (let n = 1; n < 4; n++){
-                $("#title").html("Player one it's your time");
-                $("#" + l[i] + n).addClass("hand");
+        for(let x = 0; x < blocks.length; x++){
+            for(let y = 0; y < blocks.length; y++)
+            {
+                let id = "#" + blocks[x][y];
 
-                $("#" + l[i] + n).click(function(){
-                    if(jogador == "Um"){
-                        $("#title").html("Player two it's your time");
-                        $("#" + l[i] + n).html("<h1>X</h1>");
-                        jogador = "Dois";
+                $(id).click(function(){
+                    if(positions[x][y] == ""){
+                        switch (player) {
+                            case "X":
+                                $("#title").html("Player two it's your time");
+                                $(id).html("<h1>X</h1>");
+                                $(id + " h1").addClass("player-1");
+            
+                                player = "O";
+                                break;
+                        
+                            case "O":
+                                $("#title").html("Player one it's your time");
+                                $(id).html("<h1>O</h1>");
+                                $(id + " h1").addClass("player-2");
+                                
+                                player = "X";
+                                break;
+                        }
+
+                        positions[x][y] = player;
+                        $(id).removeClass("hand");
                     }
-                    else{
-                        $("#title").html("Player one it's your time");
-                        $("#" + l[i] + n).html("<h1>O</h1>");
-                        jogador = "Um";
-                    }
-                }).off("click", "#" + l[i] + n);
+                }).addClass("hand");
             }
         }
-
         
+        player = "X";
     });
 });
